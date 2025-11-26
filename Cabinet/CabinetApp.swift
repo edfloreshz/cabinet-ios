aae4c2971747434cccfa448d19a744be3c49c294
Eduardo Flores <edfloreshz@gmail.com>
2025-11-26 07:52:13 -0700

    Initial Commit

---
+++ Cabinet/CabinetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CabinetApp.swift
+//  Cabinet
+//
+//  Created by Eduardo Flores on 26/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CabinetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cabinet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cabinet
+//
+//  Created by Eduardo Flores on 26/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
